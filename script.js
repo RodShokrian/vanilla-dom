187d655cf77806ea03e54018ee388cd7d5c78dc7
Norris Kwan <norris.kwan@gmail.com>
2017-07-05 20:41:21 -0700

    toggle and puppies

--- script.js
+++ script.js
@@ -14,6 +14,56 @@ document.addEventListener("DOMContentLoaded", () => {
     li.addEventListener("click", toggleLi);
   });
 
+  var sub = document.getElementsByClassName("list-container");
+  sub[0].addEventListener("submit", event => {
+    event.preventDefault();
+    const subInputVal = document.getElementsByClassName("favorite-input");
+    const subInput = subInputVal[0].value;
+    subInputVal.value = "";
+
+    const ul = document.getElementById("sf-places");
+    const li = document.createElement("li");
+    li.textContent = subInput;
+    ul.appendChild(li);
+  });
+
+
+  const toggleDiv = (e) => {
+    e.preventDefault();
+    const div = document.getElementById("divvy");
+    if (div.className === "photo-form-container hidden") {
+      div.className = "photo-form-container";
+    } else {
+      div.className = "photo-form-container hidden";
+    }
+  };
+
+  const hidden = document.querySelector(".photo-show-button");
+  hidden.addEventListener("click", toggleDiv);
+
+  const handlePhotoSubmit = (e) => {
+  e.preventDefault();
+
+  const photoUrlInput = document.querySelector(".photo-url-input");
+  const photoUrl = photoUrlInput.value;
+  photoUrlInput.value = "";
+
+  const newImg = document.createElement("img");
+  newImg.src = photoUrl;
+
+  const newPhotoLi = document.createElement("li");
+  newPhotoLi.appendChild(newImg);
+
+  const dogPhotosList = document.querySelector(".dog-photos");
+  dogPhotosList.appendChild(newPhotoLi);
+};
+
+const photoSubmitButton = document.querySelector(".photo-url-submit");
+photoSubmitButton.addEventListener("click", handlePhotoSubmit);
+});
+
+
+
 
 
   // adding SF places as list items
